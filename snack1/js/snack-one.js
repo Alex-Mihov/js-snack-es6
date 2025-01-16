5aefabcebcc8dbfd88b07283a98786efa354ad07
Alex Mihov <alexmihov.as@gmail.com>
2025-01-16 16:02:01 +0100

    [feat] selezione numero minore

--- snack1/js/snack-one.js
+++ snack1/js/snack-one.js
@@ -13,13 +13,24 @@ const biciDaCorsa = [
     { nome: "Bici 7", peso: 6.8 },
     { nome: "Bici 8", peso: 7.2 },
     { nome: "Bici 9", peso: 6.8 },
-    { nome: "Bici 10", peso: 6.7 }
+    { nome: "Bici 10", peso: 9.7 }
 ];
 console.log(biciDaCorsa);
 
 
-// individuare la bici con il peso minore 
-let pesoBici = biciDaCorsa.map(pesoMin => pesoMin.peso)
-
+// individuare il peso minore 
+// uso map per creare un array nuovo con i valore indicati
+let pesoBici = biciDaCorsa.map(pesoMin => pesoMin.peso);
 console.log(pesoBici);
+
+// metto a confronto gli elementi dell'array e seleziono quello minore
+const pesoMinore = pesoBici.reduce((min, pesoBici) => Math.min(min, pesoBici), pesoBici[0]);
+
+// individua la bici con il peso minore
+
+
+
+
+
+
 
