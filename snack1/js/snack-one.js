3dd0ed28018cb40f72833d30460ca26ff2ce45d8
Alex Mihov <alexmihov.as@gmail.com>
2025-01-16 15:13:49 +0100

    [feat] array con gli oggetti

--- snack1/js/snack-one.js
+++ snack1/js/snack-one.js
@@ -3,5 +3,19 @@
 
 
 // creazione array con gli oggetti
+const biciDaCorsa = [
+    { nome: "Bici 1", peso: 6.8 },
+    { nome: "Bici 2", peso: 7.2 },
+    { nome: "Bici 3", peso: 7.1 },
+    { nome: "Bici 4", peso: 6.7 },
+    { nome: "Bici 5", peso: 6.9 },
+    { nome: "Bici 6", peso: 7.0 },
+    { nome: "Bici 7", peso: 6.8 },
+    { nome: "Bici 8", peso: 7.2 },
+    { nome: "Bici 9", peso: 6.8 },
+    { nome: "Bici 10", peso: 6.7 }
+];
+console.log(biciDaCorsa);
+
 
 // individuare la bici con il peso minore 
